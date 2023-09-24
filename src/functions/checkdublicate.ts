ebb2591484b6355f0cae59dc7264231b0e044ebf
Niko Ivan <nixonivan@rambler.ru>
2023-09-24 22:31:15 +0300

    task refactored

--- src/functions/checkdublicate.ts
+++ src/functions/checkdublicate.ts
@@ -6,10 +6,10 @@ export default function checkDublicate(
 ) {
   const copy = prevList.find((el) => el.date === arg.date);
   if (copy) {
-    console.log(`copy - ${copy.dist} arg - ${arg.dist}`);
-    copy.dist += arg.dist;
-    return prevList;
-  } else {
-    return [...prevList, arg];
+    const newItem = { date: arg.date, dist: arg.dist + copy.dist };
+    const newList = prevList.filter((el) => el.date !== arg.date);
+    newList.push(newItem);
+    return newList;
   }
+  return [...prevList, arg];
 }
